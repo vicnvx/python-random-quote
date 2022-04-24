1d3a9eb6ff9878d2e8e9d4752356c63eeb39fff1
vicnvx <victormlira@outlook.com>
2022-04-24 13:27:10 -0500

    change function name

--- get-quote.py
+++ get-quote.py
@@ -1,4 +1,4 @@
-def main():
+def first():
   print("Keep it logically awesome")
   #f = open("quotes.txt")
   #quotes = f.readlines()
@@ -7,4 +7,4 @@ def main():
   # #print(quotes)
 
 if __name__== "__main__":
-  main()
+  first()
