1fd14d4dd6895965b1cdce4a190e0024c8ae7b62
vicnvx <victormlira@outlook.com>
2022-04-24 14:18:11 -0500

    multiple quotes per line

--- get-quote.py
+++ get-quote.py
@@ -1,13 +1,21 @@
 import random
+import re
 
 def first():
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
-   
-  last = 13
-  rnd = random.randint(0, last)
-  print(quotes[rnd])
+  
+  
+  last = len(quotes) - 1
+  print(adjust(quotes[rnd(last)]), adjust(quotes[rnd(last)]))
 
+def rnd(x):
+  return random.randint(0, x)
+
+def adjust(x):
+  result = re.sub('\\n', '', x)
+  return result
+  
 if __name__== "__main__":
   first()
